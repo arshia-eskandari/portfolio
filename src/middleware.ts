0dc1c788fd31f255787549ece5f2478a0377fd27
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-05-27 23:56:25 -0400

    removed is authenticated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
+import { isAuthenticated } from "@/app/admin/_actions/jwt";
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
 
-  if (!token || !(await isAuthenticated(token))) {
+  if (!token || !(await isAuthenticated(token)).isValid) {
     const url = req.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
-}
-
-async function isAuthenticated(token: string) {
-  try {
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    const { payload } = await jwtVerify(token, secret);
-
-    if (payload.role !== "admin") {
-      return false;
-    }
-
-    const currentTime = Math.floor(Date.now() / 1000);
-    if (payload.exp && payload.exp < currentTime) {
-      return false;
-    }
-
-    return true;
-  } catch (error) {
-    console.error("JWT validation error:", error);
-    return false;
-  }
 }
 
 export const config = {
