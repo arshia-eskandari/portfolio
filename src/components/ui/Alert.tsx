4128f5955fed1422fb56e5a2b91e58de475377b5
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-05-28 13:19:38 -0400

    alert

---
+++ src/components/ui/Alert.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const alertVariants = cva(
+  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  {
+    variants: {
+      variant: {
+        default: "bg-background text-foreground",
+        destructive:
+          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+function Alert(
+  {
+    className,
+    variant,
+    ...props
+  }: React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>,
+  ref: React.ForwardedRef<HTMLDivElement>,
+) {
+  return (
+    <div
+      ref={ref}
+      role="alert"
+      className={cn(alertVariants({ variant }), className)}
+      {...props}
+    />
+  );
+}
+Alert.displayName = "Alert";
+const ForwardedAlert = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(Alert);
+
+function AlertTitle(
+  { className, ...props }: React.HTMLAttributes<HTMLHeadingElement>,
+  ref: React.ForwardedRef<HTMLParagraphElement>,
+) {
+  return (
+    <h5
+      ref={ref}
+      className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+      {...props}
+    />
+  );
+}
+AlertTitle.displayName = "AlertTitle";
+const ForwardedAlertTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(AlertTitle);
+
+function AlertDescription(
+  { className, ...props }: React.HTMLAttributes<HTMLParagraphElement>,
+  ref: React.ForwardedRef<HTMLParagraphElement>,
+) {
+  return (
+    <div
+      ref={ref}
+      className={cn("text-sm [&_p]:leading-relaxed", className)}
+      {...props}
+    />
+  );
+}
+AlertDescription.displayName = "AlertDescription";
+const ForwardedAlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(AlertDescription);
+
+export {
+  ForwardedAlert as Alert,
+  ForwardedAlertTitle as AlertTitle,
+  ForwardedAlertDescription as AlertDescription,
+};
---
+++ src/components/ui/ErrorAlert.tsx
@@ -0,0 +1,24 @@
+import { AlertCircle } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "./Alert";
+import { cn } from "@/lib/utils";
+
+export function ErrorAlert({ errorMssg }: { errorMssg: string }) {
+  return (
+    <>
+      {errorMssg !== "" && (
+        <Alert
+          variant="destructive"
+          className={cn(
+            "absolute bg-red-600 text-white",
+            "left-1/2 top-[3rem] w-11/12 -translate-x-1/2 -translate-y-1/2",
+            "z-10 md:bottom-5 md:left-5 md:top-auto md:w-1/2 md:-translate-x-0 md:-translate-y-0",
+          )}
+        >
+          <AlertCircle className="h-4 w-4" color="white" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{errorMssg}</AlertDescription>
+        </Alert>
+      )}
+    </>
+  );
+}
