89f443a2c30831c0d7f146001b47b039d84cc4fb
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-05-28 20:32:10 -0400

    added remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.HOST_NAME,
+        pathname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
