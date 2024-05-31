c94eb4575c94c71a2d5eac551aa89404fd7caefa
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-05-31 11:42:02 -0400

    select fix

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -20,12 +20,15 @@ const SelectTrigger = React.forwardRef<
     <SelectPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 ring-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+        "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-1 ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       {...props}
     >
-      {children}
+      {/* EXPLANATION: Shadcn's implementation causes unintended resizing so truncate is used instead */}
+      <div className="flex justify-start overflow-hidden [&>span]:truncate">
+        {children}
+      </div>
       <SelectPrimitive.Icon asChild>
         <ChevronDown className="h-4 w-4 opacity-50" />
       </SelectPrimitive.Icon>
