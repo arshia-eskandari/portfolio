82716e770e0a81b1410d1008fa05cf5a36614956
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-06-01 19:26:18 -0400

    time formatter

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -5,3 +5,15 @@ export function timeTester(seconds: number) {
     }, seconds * 1000);
   });
 }
+
+export function formatReadableDate(date: Date): string {
+  return date.toLocaleString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+}
