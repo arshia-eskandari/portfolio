72f1c591aeafdb6f2c84027fc1c65370b42b0599
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-06-05 13:01:27 -0400

    update action

--- src/app/admin/experiences/_actions/experiences.ts
+++ src/app/admin/experiences/_actions/experiences.ts
@@ -1,6 +1,7 @@
 "use server";
 import db from "@/db/db";
 import { addDays } from "date-fns";
+import { z } from "zod";
 
 export async function getExperiences() {
   try {
@@ -34,5 +35,82 @@ export async function addDefaultExperience() {
   } catch (error) {
     console.log("🚀 ~ addDefaultExperience ~ error:", error);
     return { status: 500, message: "Failed to upload about details" };
+  }
+}
+
+const updateSchema = z.object({
+  id: z.string().max(1000),
+  jobTitle: z.string().min(5).max(30),
+  company: z.string().min(5).max(30),
+  location: z.string().min(5).max(30),
+  achievements: z.string().min(0).max(1000),
+  responsibilities: z.string().min(0).max(1000),
+  startDate: z.string().min(10).max(100),
+  endDate: z.string().max(100),
+  recommendationLetterUrls: z.string().min(0).max(1500),
+});
+
+export async function updateExperience(formData: FormData) {
+  try {
+    const result = updateSchema.safeParse(
+      Object.fromEntries(formData.entries()),
+    );
+    if (!result.success) {
+      return {
+        status: 400,
+        message: "Invalid experience details",
+      };
+    }
+    const {
+      id,
+      jobTitle,
+      company,
+      location,
+      achievements,
+      responsibilities,
+      startDate,
+      endDate,
+      recommendationLetterUrls,
+    } = result.data;
+
+    for (const url of recommendationLetterUrls.split(",")) {
+      if (!db.media.findFirst({ where: { url } })) {
+        return {
+          status: 404,
+          message: "Url not found",
+        };
+      }
+    }
+
+    await db.experience.update({
+      where: {
+        id,
+      },
+      data: {
+        jobTitle: jobTitle.trim(),
+        company: company.trim(),
+        location: location.trim(),
+        achievements:
+          achievements
+            ?.split(",")
+            .map((a) => a.trim())
+            .filter((a) => a !== "") || [],
+        responsibilities:
+          responsibilities
+            ?.split(",")
+            .map((r) => r.trim())
+            .filter((r) => r !== "") || [],
+        startDate: new Date(startDate),
+        endDate: endDate === "" ? null : new Date(endDate),
+        recommendationLetterUrls: recommendationLetterUrls?.split(",") || [],
+      },
+    });
+    return {
+      status: 200,
+      message: "Experience details successfully updated",
+    };
+  } catch (error) {
+    console.log("🚀 ~ updateExperience ~ error:", error);
+    return { status: 500, message: "Failed to update experience details" };
   }
 }
