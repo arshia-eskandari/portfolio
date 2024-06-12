0b02142d3634712de538a6056ada8b634d4b4880
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-06-12 00:44:13 -0400

    added sliding animation

--- src/app/(clientFacing)/_components/MediaCarousel.tsx
+++ src/app/(clientFacing)/_components/MediaCarousel.tsx
@@ -1,5 +1,3 @@
-// components/MediaCarousel.tsx
-import Image from "next/image";
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
 
@@ -15,42 +13,45 @@ const MediaCarousel: React.FC<MediaCarouselProps> = ({ media }) => {
   };
 
   const handlePrev = () => {
-    setCurrentMediaIndex(
-      (prevIndex) => (prevIndex - 1 + media.length) % media.length,
-    );
-  };
-
-  const renderMedia = (mediaUrl: string) => {
-    if (mediaUrl.match(/\.(jpeg|jpg|png)$/i)) {
-      return (
-        <div
-          style={{
-            backgroundImage: `url(${mediaUrl})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "100%",
-            width: "100%",
-          }}
-        ></div>
-      );
-    } else if (mediaUrl.endsWith(".mp4")) {
-      return (
-        <div className="flex h-[500px] w-full items-center justify-center">
-          <ReactPlayer
-            url={mediaUrl}
-            controls={true}
-            width="100%"
-            height="100%"
-            style={{ backgroundColor: "black", alignSelf: "center" }}
-          />
-        </div>
-      );
-    }
+    setCurrentMediaIndex((prevIndex) => {
+      return (prevIndex - 1 + media.length) % media.length;
+    });
   };
 
   return (
     <div className="relative h-[500px] w-1/2 overflow-hidden rounded-sm">
-      {renderMedia(media[currentMediaIndex])}
+      <div
+        className="flex transition-transform duration-500 ease-in-out"
+        style={{
+          transform: `translateX(-${currentMediaIndex * 100}%)`,
+        }}
+      >
+        {media.map((mediaUrl) => (
+          <div key={mediaUrl} className="h-[500px] min-w-full flex-shrink-0">
+            {mediaUrl.match(/\.(jpeg|jpg|png)$/i) ? (
+              <div
+                style={{
+                  backgroundImage: `url(${mediaUrl})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  height: "100%",
+                  width: "100%",
+                }}
+              ></div>
+            ) : (
+              <div className="flex h-full w-full items-center justify-center">
+                <ReactPlayer
+                  url={mediaUrl}
+                  controls={true}
+                  width="100%"
+                  height="100%"
+                  style={{ backgroundColor: "black", alignSelf: "center" }}
+                />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
       <button
         className="absolute left-0 top-1/2 z-10 -translate-y-1/2 transform rounded-full bg-white p-2 shadow-lg"
         onClick={handlePrev}
@@ -63,6 +64,9 @@ const MediaCarousel: React.FC<MediaCarouselProps> = ({ media }) => {
       >
         &gt;
       </button>
+      <div className="absolute bottom-5 left-5 z-20 text-white">
+        {currentMediaIndex + 1}/{media.length}
+      </div>
     </div>
   );
 };
