65a8ddbad3cdf2413c566ed937be1fdbe8e595dd
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-06-13 23:40:00 -0400

    removed wavy background

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "./_components/LoginForm";
 
 export default async function Login() {
   return (
-    <div className="wavy-background flex h-screen w-full items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <LoginForm action={login} />
     </div>
   );
