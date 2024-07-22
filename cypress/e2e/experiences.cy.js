0d0720bd10748451273c9c88f9c86c69711b89e0
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-07-22 08:47:17 -0400

    added experiences test

---
+++ cypress/e2e/experiences.cy.js
@@ -0,0 +1,22 @@
+describe("Experiences Section Tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/#experiences");
+  });
+  it("Opens and closes the accordion correctly", () => {
+    cy.get("#experiences>div>div:nth-child(1)>div")
+      .click()
+      .should("have.attr", "data-state", "open");
+    cy.get("#experiences>div>div:nth-child(1)>div>div")
+      .should("be.visible")
+      .should("have.attr", "data-state", "open");
+
+    cy.wait(300);
+
+    cy.get("#experiences>div>div:nth-child(1)>div>h3")
+      .click()
+      .should("have.attr", "data-state", "closed");
+    cy.get("#experiences>div>div:nth-child(1)>div>div")
+      .should("be.hidden")
+      .should("have.attr", "data-state", "closed");
+  });
+});
