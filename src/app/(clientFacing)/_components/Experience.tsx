1100246e2f88b8d3e68e87b11f8516c733a3c058
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2025-09-01 19:54:12 -0400

    fixed area of effect for clicking

--- src/app/(clientFacing)/_components/Experience.tsx
+++ src/app/(clientFacing)/_components/Experience.tsx
@@ -31,60 +31,58 @@ export default function ExperienceDetails({
   };
 
   return (
-    <div>
-      <AccordionItem
-        id={`div-${experience.id}`}
-        value={experience.id}
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchStart}
-        className={cn(
-          "sm:animated-button sm:shine my-6 rounded-2xl bg-[#050041] p-3 text-white",
-          additionalClasses,
+    <AccordionItem
+      id={`div-${experience.id}`}
+      value={experience.id}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchStart}
+      className={cn(
+        "sm:animated-button sm:shine my-6 rounded-2xl bg-[#050041] text-white",
+        additionalClasses,
+      )}
+    >
+      <AccordionTrigger className="no-underline py-6 px-3" id={experience.id}>
+        {`${experience.jobTitle}  |  ${experience.company}  |  ${
+          experience.location
+        }  |  ${formatDate(new Date(experience.startDate))} - ${
+          experience.endDate
+            ? formatDate(new Date(experience.endDate))
+            : "Present"
+        }`}
+      </AccordionTrigger>
+      <AccordionContent className="px-3" id={`content-${experience.id}`}>
+        <P className="font-bold">Experience Details</P>
+        <P>Achievements</P>
+        <ul className="ml-6 list-disc">
+          {experience.achievements.map((a) => {
+            return <li key={a}>{capitalizeFirstLetter(a)}</li>;
+          })}
+        </ul>
+        <P>Reponsibilities</P>
+        <ul className="ml-6 list-disc">
+          {experience.responsibilities.map((a) => {
+            return <li key={a}>{capitalizeFirstLetter(a)}</li>;
+          })}
+        </ul>
+        {experience?.recommendationLetterUrls?.length > 0 && (
+          <>
+            <P>Recommendation Letters</P>
+            <ul className="ml-6 list-disc">
+              {experience.recommendationLetterUrls.map((url, i) => (
+                <li key={url}>
+                  <a
+                    key={url}
+                    href={url}
+                    // EXPLANATION: When clicking on the checkbox the modal should not appear
+                    onClick={(e) => e.stopPropagation()}
+                    className="flex underline"
+                  >{`Recommendation Letter ${i + 1}`}</a>
+                </li>
+              ))}
+            </ul>
+          </>
         )}
-      >
-        <AccordionTrigger className="no-underline" id={experience.id}>
-          {`${experience.jobTitle}  |  ${experience.company}  |  ${
-            experience.location
-          }  |  ${formatDate(new Date(experience.startDate))} - ${
-            experience.endDate
-              ? formatDate(new Date(experience.endDate))
-              : "Present"
-          }`}
-        </AccordionTrigger>
-        <AccordionContent className="px-3" id={`content-${experience.id}`}>
-          <P className="font-bold">Experience Details</P>
-          <P>Achievements</P>
-          <ul className="ml-6 list-disc">
-            {experience.achievements.map((a) => {
-              return <li key={a}>{capitalizeFirstLetter(a)}</li>;
-            })}
-          </ul>
-          <P>Reponsibilities</P>
-          <ul className="ml-6 list-disc">
-            {experience.responsibilities.map((a) => {
-              return <li key={a}>{capitalizeFirstLetter(a)}</li>;
-            })}
-          </ul>
-          {experience?.recommendationLetterUrls?.length > 0 && (
-            <>
-              <P>Recommendation Letters</P>
-              <ul className="ml-6 list-disc">
-                {experience.recommendationLetterUrls.map((url, i) => (
-                  <li key={url}>
-                    <a
-                      key={url}
-                      href={url}
-                      // EXPLANATION: When clicking on the checkbox the modal should not appear
-                      onClick={(e) => e.stopPropagation()}
-                      className="flex underline"
-                    >{`Recommendation Letter ${i + 1}`}</a>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-        </AccordionContent>
-      </AccordionItem>
-    </div>
+      </AccordionContent>
+    </AccordionItem>
   );
 }
